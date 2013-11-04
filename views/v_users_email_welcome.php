48d191e74949fb2314b85e606b0723f9a0236293
reenuchahal <reenuchahal@gmail.com>
2013-11-04 15:23:37 -0500

    Email Body Modification: styling

--- views/v_users_email_welcome.php
+++ views/v_users_email_welcome.php
@@ -1,4 +1,4 @@
-<div class="alert alert-warning">
+<div style="background-color:#f6f6f6; padding: 20px; margin-top:10px;">
 <h3>Hello <?=$_POST['first_name'];?> <?=$_POST['last_name'];?>,</h3>
 <p> Thank you for signing up at ChitChat. Your may login to <a href="http://p2.buildweb-id.com/users/login/">ChitChat</a> Using the following email:<br/><br/>
 Email: <?=$_POST['email'];?><br/><br/>
