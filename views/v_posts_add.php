280091ab918262f90ca9065c55c3d455691a2b1b
reenuchahal <reenuchahal@gmail.com>
2013-11-05 18:34:16 -0500

    v_posts_add.php Formatting

--- views/v_posts_add.php
+++ views/v_posts_add.php
@@ -3,40 +3,47 @@
 		<form method="post" action="/posts/p_add">
 			<label for='content'>Update Status</label><br>
 		    <textarea class="form-control" rows="3" name='content' id='content' placeholder="What's on your mind?" required></textarea><br/>
+		    
 		    <div class="text-right">
-		    <button  type="submit" class="btn btn-primary">Post</button>
+		    	<button  type="submit" class="btn btn-primary">Post</button>
 		    </div>
 		</form>
 		<h1>ChitChat</h1>
 		<br/>
-	</div>
-</div>
+	</div> <!-- / .col-md-offset-2 .col-xs-12 .col-sm-10 .col-md-8  -->
+</div> <!-- / .row -->
+
 <?php $loggedInUser= $user->first_name?>
+
 <?php foreach($posts as $post): ?>
-<div class="row">
-	<div class="col-md-offset-2 col-xs-12 col-sm-10 col-md-8">
-		<div class="feed-display">
-			<h3><?=$post['first_name']?> <?=$post['last_name']?></h3>
-			<p><?=$post['content']?></p>
-			<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
-				<?=Time::display($post['created'])?>
-			</time>
+	<div class="row">
+		<div class="col-md-offset-2 col-xs-12 col-sm-10 col-md-8">
+			<div class="feed-display">
 			
-			<div class="text-right delete-btn">
-				<?php if($post['first_name'] == $loggedInUser): ?>
-					<a href="/posts/delete/<?=$post['post_id']?>" class="btn btn-danger btn-xs" >Delete</a>
-					<a href="/posts/edit/<?=$post['post_id']?>" class="btn btn-default btn-xs" >Edit</a>
-				<?php endif; ?>
-				<?php if(isset($connections[$post['post_id']])): ?>
-					<a href='/posts/unlike/<?=$post['post_id']?>' class="btn btn-default btn-xs">Unlike</a>
-				<?php else: ?>			
-					<a href='/posts/like/<?=$post['post_id']?>' class="btn btn-default btn-xs">Like</a>
-				<?php endif; ?>
-				<?php if(isset($count[$post['post_id']]['count'])): ?>
-				 	<img src="/uploads/Facebook-Thumbs-Up.jpg" alt="facebook thumbs up"/> (<?php echo $count[$post['post_id']]['count'] ?>)
-				<?php endif; ?>
-			</div><!-- / .text-right -->
-		</div>
-	</div>
-</div>
+				<h3><?=$post['first_name']?> <?=$post['last_name']?></h3>
+				<p><?=$post['content']?></p>
+				<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+					<?=Time::display($post['created'])?>
+				</time>
+				
+				<div class="text-right delete-btn">
+					<?php if($post['first_name'] == $loggedInUser): ?>
+						<a href="/posts/delete/<?=$post['post_id']?>" class="btn btn-danger btn-xs" >Delete</a>
+						<a href="/posts/edit/<?=$post['post_id']?>" class="btn btn-default btn-xs" >Edit</a>
+					<?php endif; ?>
+					
+					<?php if(isset($connections[$post['post_id']])): ?>
+						<a href='/posts/unlike/<?=$post['post_id']?>' class="btn btn-default btn-xs">Unlike</a>
+					<?php else: ?>			
+						<a href='/posts/like/<?=$post['post_id']?>' class="btn btn-default btn-xs">Like</a>
+					<?php endif; ?>
+					
+					<?php if(isset($count[$post['post_id']]['count'])): ?>
+					 	<img src="/uploads/Facebook-Thumbs-Up.jpg" alt="facebook thumbs up"/> (<?php echo $count[$post['post_id']]['count'] ?>)
+					<?php endif; ?>
+				</div><!-- / .text-right -->
+				
+			</div> <!-- / .feed-display -->
+		</div> <!-- / .col-md-offset-2 .col-xs-12 .col-sm-10 .col-md-8 -->
+	</div> <!-- / .row -->
 <?php endforeach;?>
