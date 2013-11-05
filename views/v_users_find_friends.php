91171b38401930719cb2faa51a4398bd74c65c51
reenuchahal <reenuchahal@gmail.com>
2013-11-05 18:50:02 -0500

    v_users_find_friends.php / @connection modifications

--- views/v_users_find_friends.php
+++ views/v_users_find_friends.php
@@ -3,16 +3,18 @@
 		<h1>Find a Connection 
 			<br/><small><?=$user->first_name;?> <?=$user->last_name;?> (<?php echo count($connections);?>)</small>
 		</h1>
+		
 		<table class="table table-hover">
 			<?php $loggedInUser = $user->user_id  ; ?>
 			
 			<?php foreach($users as $user): ?>
 				<tr <?php if ($loggedInUser == $user['user_id']): ?> class="active" <?php endif; ?>>
+					
 					<td class="follow-width"><?=$user['first_name']?> <?=$user['last_name']?> <td>
+					
 					<!-- If there exists a connection with this user, show a unfollow link -->
 					<td class="follow-width">
-					
-					    <?php if(isset($connections[$user['user_id']])  ): ?>
+						<?php if(isset($connections[$user['user_id']])  ): ?>
 							<a href='/posts/unfollow/<?=$user['user_id']?>' class="btn btn-danger">Unfollow</a>
 						
 						<!-- Otherwise, show the follow link -->
@@ -21,8 +23,9 @@
 						
 						<?php endif; ?>
 					</td>
+					
 				</tr>
 			<?php endforeach; ?>
 		</table>
-	</div>
-</div>
+	</div> <!-- / .col-md-offset-2 .col-sm-offset-2 .col-xs-12 .col-sm-8 .col-md-8 -->
+</div> <!-- / .row -->
