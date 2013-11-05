541a61ce69dfbdfbaefead0b4c0abd4b81b01c1e
reenuchahal <reenuchahal@gmail.com>
2013-11-05 16:22:52 -0500

    formatting on c_posts.php

--- controllers/c_posts.php
+++ controllers/c_posts.php
@@ -21,20 +21,20 @@ class posts_controller extends base_controller {
 		
 		#Build the Query
 		$q ='SELECT
-			posts.post_id,
-            posts.content,
-            posts.created,
-            posts.user_id AS post_user_id,
-            users_users.user_id AS follower_id,
-            users.first_name,
-            users.last_name
-        FROM posts
-		INNER JOIN users_users
+				posts.post_id,
+				posts.content,
+				posts.created,
+				posts.user_id AS post_user_id,
+				users_users.user_id AS follower_id,
+				users.first_name,
+				users.last_name
+        	FROM posts
+			INNER JOIN users_users
             ON posts.user_id = users_users.user_id_followed
-        INNER JOIN users
+        	INNER JOIN users
             ON posts.user_id = users.user_id
-		WHERE users_users.user_id = '.$this->user->user_id.' 
-		ORDER BY posts.created DESC';
+			WHERE users_users.user_id = '.$this->user->user_id.' 
+			ORDER BY posts.created DESC';
 		
 		#Run the Query
 		$posts = DB::instance(DB_NAME)->select_rows($q);
