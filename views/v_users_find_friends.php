2035f4a1290acb1ee216042c9b1cdfb2d5a90fff
reenuchahal <reenuchahal@gmail.com>
2013-11-05 18:53:44 -0500

    v_users_find_friends.php / @connection modifications

--- views/v_users_find_friends.php
+++ views/v_users_find_friends.php
@@ -1,6 +1,7 @@
 <div class="row">
 	<div class="col-md-offset-2 col-sm-offset-2 col-xs-12 col-sm-8 col-md-8">
-		<h1>Find a Connection<br/> 
+		
+		<h1>Find a Connection <br/> 
 			<small><?=$user->first_name;?> <?=$user->last_name;?> (<?php echo count($connections);?>)</small>
 		</h1>
 		
@@ -9,7 +10,6 @@
 			
 			<?php foreach($users as $user): ?>
 				<tr <?php if ($loggedInUser == $user['user_id']): ?> class="active" <?php endif; ?>>
-					
 					<td class="follow-width"><?=$user['first_name']?> <?=$user['last_name']?><td>
 					
 					<td class="follow-width">
@@ -24,9 +24,9 @@
 						<?php endif; ?>
 						
 					</td>
-					
 				</tr>
 			<?php endforeach; ?>
 		</table>
+		
 	</div> <!-- / .col-md-offset-2 .col-sm-offset-2 .col-xs-12 .col-sm-8 .col-md-8 -->
 </div> <!-- / .row -->
