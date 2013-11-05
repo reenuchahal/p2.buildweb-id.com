5b693b115e1c80fb86f0cac5aabed73502483c2b
reenuchahal <reenuchahal@gmail.com>
2013-11-05 18:52:30 -0500

    v_users_find_friends.php / @connection modifications

--- views/v_users_find_friends.php
+++ views/v_users_find_friends.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<div class="col-md-offset-2 col-sm-offset-2 col-xs-12 col-sm-8 col-md-8">
-		<h1>Find a Connection 
-			<br/><small><?=$user->first_name;?> <?=$user->last_name;?> (<?php echo count($connections);?>)</small>
+		<h1>Find a Connection<br/> 
+			<small><?=$user->first_name;?> <?=$user->last_name;?> (<?php echo count($connections);?>)</small>
 		</h1>
 		
 		<table class="table table-hover">
@@ -10,18 +10,19 @@
 			<?php foreach($users as $user): ?>
 				<tr <?php if ($loggedInUser == $user['user_id']): ?> class="active" <?php endif; ?>>
 					
-					<td class="follow-width"><?=$user['first_name']?> <?=$user['last_name']?> <td>
+					<td class="follow-width"><?=$user['first_name']?> <?=$user['last_name']?><td>
 					
-					<!-- If there exists a connection with this user, show a unfollow link -->
 					<td class="follow-width">
+						
+						<!-- If there exists a connection with this user, show a unfollow link -->
 						<?php if(isset($connections[$user['user_id']])  ): ?>
 							<a href='/posts/unfollow/<?=$user['user_id']?>' class="btn btn-danger">Unfollow</a>
 						
 						<!-- Otherwise, show the follow link -->
 						<?php else: ?>
 							<a href='/posts/follow/<?=$user['user_id']?>' class="btn btn-success">Follow</a>
-						
 						<?php endif; ?>
+						
 					</td>
 					
 				</tr>
