c4306efba7e913307c860a300c949e2d093bfbdb
reenuchahal <reenuchahal@gmail.com>
2013-11-06 11:30:06 -0500

    route /posts/edit/ to /posts/add/

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
 # Routing
 # You can use regexp, for example '/users/([A-Za-z0-9-\s\@]+)' => '/members/$0',
     Router::$routes = array(
-    	'/' => '/index'     # default controller when "/" is requested
+    	'/' => '/index',     # default controller when "/" is requested
+    	'/posts/edit/' => '/posts/add/'
     );
     
 # Match requested uri to any routes and instantiate controller
